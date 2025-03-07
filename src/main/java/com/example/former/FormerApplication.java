2e619efcaa7b66b0bd4b4caab3d051e50ef8c1b0
mrchar <mrchar@qq.com>
2025-03-07 21:05:00 +0800

    创建项目

---
+++ src/main/java/com/example/former/FormerApplication.java
@@ -0,0 +1,13 @@
+package com.example.former;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/former/FormerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.former;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
