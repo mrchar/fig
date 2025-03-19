0b61d2d37dd7ddf25412bdd33dbdb05c4d395ab2
mrchar <mrchar@qq.com>
2025-03-19 21:28:49 +0800

    安装auto-import和vue-components

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), AutoImport({}), Components({})],
 })
